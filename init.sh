cd091b9ba0d43d3532a687b67a85d85fd470a0da
Seongpyo Hong <sphong0417@gmail.com>
2020-11-07 03:17:04 +0900

    Add Argo CD

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-GCR_PATH={GCR_PATH}
+GCP_CREDENTIAL_PATH={GCP_CREDENTIAL_PATH}
 # Create Namespace
 kubectl create ns argo
 kubectl create ns argo-events
+kubectl create ns argocd
 
 # Install CR For Argo Events
 kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/namespace-install.yaml
@@ -11,8 +12,13 @@ kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/stable/m
 # Depoly Event Bus
 kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/eventbus/native.yaml
 
-# Install CR For Argo
+# Install CR For Argo & expose argo-server
 kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
+kubectl patch svc argo-server -n argo -p '{"spec": {"type": "LoadBalancer"}}'
+
+# Install CR For Argo CD & expose argocd-server
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # Set Secret
 ## Github Access
@@ -20,7 +26,7 @@ kubectl apply -f secret/github-access-argo-event.yaml
 kubectl apply -f secret/github-access-argo-workflow.yaml
 
 ## GCR Credentials
-kubectl create secret generic argo-gcp --namespace=argo --from-file=${GCR_PATH}/google.json
+kubectl create secret generic argo-gcp --namespace=argo --from-file=google.json=${GCP_CREDENTIAL_PATH}
 
 # Set Cluster Role & Cluster Role Binding
 kubectl apply -f argo-clusterrole.yaml
@@ -28,6 +34,7 @@ kubectl apply -f argo-clusterrole.yaml
 ## Deploy Key For Github Repository
 kubectl apply -f secret/source-key-argo.yaml
 kubectl apply -f secret/deploy-key-argo.yaml
+kubectl apply -f secret/deploy-key-argocd.yaml
 
 # Edit Workflow Controller Artifact to use GCS
 kubectl edit configmap workflow-controller-configmap -n argo
@@ -41,3 +48,9 @@ kubectl apply -f event-sensor.yaml
 
 # Deploy Workflow template
 kubectl apply -f workflow-template.yaml
+
+# Set Repository Information
+kubectl apply -f repository.yaml
+
+# Create Application Template
+kubectl apply -f application.yaml
